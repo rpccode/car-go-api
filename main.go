ccc0e7c8a6c6d60a69c41121178528c0dd15628b
Rudy Casilla <94497674+rpccode@users.noreply.github.com>
2024-10-06 18:32:59 -0400

    pagina publica

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"go-auth-api/src/config"
 	routes "go-auth-api/src/router"
 	"log"
+	"net/http"
 
 	// Importar el nuevo archivo de rutas
 
@@ -24,6 +26,8 @@ func main() {
 
 	r := gin.Default()
 
+	http.HandleFunc("/", homePage)
+
 	// Configurar CORS
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},                                       // Agrega los orígenes permitidos
@@ -39,4 +43,21 @@ func main() {
 	// Ejecutar el servidor en el puerto 3000
 	log.Println("Iniciando servidor HTTPS en http://192.168.0.104:3000")
 	r.Run(":3000")
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bienvenido a mi API</title>
+    </head>
+    <body>
+        <h1>Bienvenido a mi API</h1>
+        <p>Esta es la página pública de la API.</p>
+    </body>
+    </html>
+    `)
 }
